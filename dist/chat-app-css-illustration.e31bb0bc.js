f183ac0acb829488e371ecce7e6c442482538ff5
xFreshu <lprzybyszcontact@gmail.com>
2020-12-13 22:10:59 +0100

    added basic scss

--- dist/chat-app-css-illustration.e31bb0bc.js
+++ dist/chat-app-css-illustration.e31bb0bc.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51976" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50432" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
